01e56d0910d4eb63cdf9e03569d8c0e668b3463e
amunram2608 <amunram2608@g.educaand.es>
2025-01-16 22:47:55 +0100

    Commit prueba

---
+++ src/ejer01_1/Producto.java
@@ -0,0 +1,39 @@
+package ejer01_1;
+
+public class Producto {
+	
+	//Atributos
+	private String modelo;
+	private byte stock;
+	private double precio;
+	
+	
+	//Get/Set
+	String getModelo() {
+		return modelo;
+	}
+	
+	void setModelo(String modelo) {
+		if() {
+			
+		}
+		
+		this.modelo = modelo;
+	}
+	
+	byte getStock() {
+		return stock;
+	}
+	
+	void setStock(byte stock) {
+		this.stock = stock;
+	}
+	
+	double getPrecio() {
+		return precio;
+	}
+	
+	void setPrecio(double precio) {
+		this.precio = precio;
+	}
+}
